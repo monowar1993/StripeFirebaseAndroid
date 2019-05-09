e554622468c69d4828cc1dac4778d117cacb7ad3
Mostafa Monowar <monowar1993@bs-23.net>
2019-05-09 17:14:32 +0600

    stripe added

---
+++ app/src/main/java/com/stripefirebase/android/AddCardActivity.kt
@@ -0,0 +1,60 @@
+package com.stripefirebase.android
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
+import com.stripe.android.Stripe
+import com.stripe.android.TokenCallback
+import com.stripe.android.model.Card
+import com.stripe.android.model.Token
+import dmax.dialog.SpotsDialog
+import kotlinx.android.synthetic.main.activity_add_card.*
+import kotlinx.android.synthetic.main.content_add_card.*
+
+class AddCardActivity : AppCompatActivity() {
+
+    companion object {
+        const val TAG = "AddCardActivity"
+    }
+
+    private val stripe by lazy { Stripe(applicationContext, BuildConfig.STRIPE_PUBLISHABLE_KEY) }
+
+    private val progressDialog by lazy { SpotsDialog.Builder().setContext(this).build() }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_card)
+
+        setSupportActionBar(toolbar)
+        supportActionBar?.let { actionBar ->
+            actionBar.setHomeButtonEnabled(true)
+            actionBar.setDisplayHomeAsUpEnabled(true)
+        }
+
+        btnGenerateToken.setOnClickListener { view ->
+            val card = cardWidget.card
+            if (card == null) {
+                Snackbar.make(view, "Invalid Card Data", Snackbar.LENGTH_LONG).show()
+            } else {
+                createToken(card)
+            }
+        }
+    }
+
+    private fun createToken(card: Card) {
+        progressDialog.show()
+        stripe.createToken(card, object : TokenCallback {
+            override fun onSuccess(token: Token) {
+                progressDialog.dismiss()
+                Log.d(TAG, "Token Id - ${token.id}")
+            }
+
+            override fun onError(error: Exception) {
+                progressDialog.dismiss()
+                error.printStackTrace()
+                Snackbar.make(layoutRoot, error.toString(), Snackbar.LENGTH_LONG).show()
+            }
+        })
+    }
+}
--- app/src/main/java/com/stripefirebase/android/MainActivity.kt
+++ app/src/main/java/com/stripefirebase/android/MainActivity.kt
@@ -1,11 +1,10 @@
 package com.stripefirebase.android
 
+import android.content.Intent
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity;
 import android.view.Menu
 import android.view.MenuItem
-
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -15,9 +14,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+        fabAdd.setOnClickListener {
+            val intent = Intent(this, AddCardActivity::class.java)
+            startActivity(intent)
         }
     }
 
