2176290afcc70136d95e7253832f000331bd2140
Mostafa Monowar <monowar1993@bs-23.net>
2019-05-17 17:53:12 +0600

    retrieve stripe token from token Id added

--- app/src/main/java/com/stripefirebase/android/PaymentActivity.kt
+++ app/src/main/java/com/stripefirebase/android/PaymentActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.functions.FirebaseFunctions
+import com.stripe.android.model.Token
 import dmax.dialog.SpotsDialog
 import kotlinx.android.synthetic.main.activity_payment.*
 
@@ -35,7 +36,31 @@ class PaymentActivity : AppCompatActivity() {
 
         tvToken.text = ("Your card token ID - %s. Use this token to make payment on stripe. Stripe Charge API is being used here.").format(cardTokenId)
 
+        btnRetrieveToken.setOnClickListener { retrieveToken() }
+
         btnPay.setOnClickListener { pay() }
+    }
+
+    private fun retrieveToken() {
+        progressDialog.show()
+        val data = hashMapOf(
+            "token_id" to cardTokenId
+        )
+        firebaseFunctions.getHttpsCallable("retrieveStripeToken").call(data).addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                val httpsCallableResult = task.result
+                val responseData = httpsCallableResult?.data as Map<*, *>
+                Log.d(TAG, responseData.toString())
+                Log.d(TAG, responseData["token"].toString())
+                val token = Token.fromString(responseData["token"].toString())
+                Log.d(TAG, token?.id)
+                Log.d(TAG, token?.card?.last4)
+                progressDialog.dismiss()
+            } else {
+                task.exception?.printStackTrace()
+                progressDialog.dismiss()
+            }
+        }
     }
 
     private fun pay() {
